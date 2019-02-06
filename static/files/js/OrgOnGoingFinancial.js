2a0bc91955ea7a8f1c244f8de8d0794b878d0000
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-06 23:30:10 +0330

    YESSSSS

--- static/files/js/OrgOnGoingFinancial.js
+++ static/files/js/OrgOnGoingFinancial.js
@@ -1,8 +1,7 @@
 $(function () {
-    alert("salam5");
     var user_name = "ehsan"; //TODO
     $.ajax({
-        url: 'http://127.0.0.1:8000/projects/benefactor/' + user_name + '/?type=financial&status=in_progress/',
+        url: 'http://127.0.0.1:8000/projects/benefactor/' + user_name + '/?type=financial&status=in_progress',
         // data: "type=financial" +
         //     "&status=in_progress/",
         type: 'GET',
@@ -10,26 +9,19 @@ $(function () {
         // jsonp: "callback",
         contentType: 'application/json',
         success: function (data) {
-            alert("success");
-            // data = $.parseJSON(data);
-            alert(data);
-
             if (data.status == 0) {
-                alert("status is 0");
                 projects = data.projects;
-                for (var project in projects) {
+
+                for (var i = 0; i < projects.length;i++) {
+                    project = projects[i];
                     var row = '<tr>';
                     row += '<td>' + project.name + '</td>';
-                    row += '</tr>';
 
                     row += '<td>' + project.location + '</td>';
-                    row += '</tr>';
 
                     row += '<td>' + project.deadline + '</td>';
-                    row += '</tr>';
 
                     row += '<td>' + project.money_needed + '</td>';
-                    row += '</tr>';
 
                     row += '<td>' + project.money_donated + '</td>';
                     row += '</tr>';
