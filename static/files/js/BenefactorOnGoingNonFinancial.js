1526e392c390c793ab89ec3bcb2b71c408d8554f
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-06 19:57:28 +0330

    Complete SingUp UI

--- static/files/js/BenefactorOnGoingNonFinancial.js
+++ static/files/js/BenefactorOnGoingNonFinancial.js
@@ -12,9 +12,19 @@ function sendBenefactorInformation() {
         type: 'post',
         dataType: 'json',
         contentType: 'application/json',
+        data: JSON.stringify(request),
         success: function (data) {
-            $('').html(data.msg);
-        },
-        data: JSON.stringify(request)
+
+
+        }
     });
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+
+        }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
 }
