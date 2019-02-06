f927a86e2cebe7420ca65c5c7ec7fcb16041836a
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-06 17:55:13 +0330

    Complete Benefactor register

--- static/files/js/RegisterBenefactor.js
+++ static/files/js/RegisterBenefactor.js
@@ -1,21 +1,26 @@
 function sendBenefactorInformation() {
+    skills = [];
+    i = 0;
+    $("#skills").find("button").each(function () {
+         skills[i++] = $(this).html();
+    });
+    alert(skills);
     var person = {
-        uesrname: $("#username").val(),
+        username: $("#username").val(),
         password1: $("#password1").val(),
         name: $("#name").val(),
         lastname: $("#lastname").val(),
-        phoneNumber:$("#phoneNumber").val(),
-        homeNumber:$("#homeNumber").val(),
-        address:$("#address").val(),
-        skills: "For later!",
+        phoneNumber: $("#phoneNumber").val(),
+        homeNumber: $("#homeNumber").val(),
+        address: $("#address").val(),
+        skills: skills,
         activities: $("#activities").val(),
         interests: $("#interests").val()
-    }
+    };
 
     $('#target').html('sending..');
-
     $.ajax({
-        url: '/test/PersonSubmit',
+        url: '/Benefactor/' + person.username,
         type: 'post',
         dataType: 'json',
         contentType: 'application/json',
@@ -24,4 +29,5 @@ function sendBenefactorInformation() {
         },
         data: JSON.stringify(person)
     });
+    console.log(JSON.stringify(person))
 }
