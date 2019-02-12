88215fe0743fb5aeba9386ae526de6dfacbd3275
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-13 02:00:34 +0330

    Add functions for admin user approval

--- static/files/js/AdminAccDeclineBenefactors.js
+++ static/files/js/AdminAccDeclineBenefactors.js
@@ -32,7 +32,7 @@ $(function () {
 });
 
 function fillBenefactorTable(benefactors) {
-    for (var i = 0; i < benefactors.length;i++) {
+    for (var i = 0; i < benefactors.length; i++) {
         var benefactor = benefactors[i];
         addBenefactor(benefactor);
     }
@@ -55,15 +55,82 @@ function addBenefactor(benefactor) {
         skillsStr += benefactor.skills[i].category + "-" + benefactor.skills[i].name;
     }
     row += '<td>' + skillsStr + '</td>';
+    row += '<button class="ui red button" id="verify">رد کاربر</button>';
+    row += '<button class="ui green button" id="deny">تایید کاربر</button>';
     row += '</tr>';
-    $('#benefactors').append(row)
+    $('#benefactors').append(row);
+    $("#verify").click(function () {
+        verifyUser(benefactor);
+    });
+    $("#deny").click(function () {
+        denyUser(benefactor);
+    });
 }
 
-/*<td><a href="BenefactorFeedbackToOrg.html">
-                                <button class="ui violet button">ارسال نظر</button>
-                            </a>
-                        </td>
-*
-*
-*
-* */
+function denyUser(benefactor) {
+    $.ajax({
+        async: true,
+        url: 'http://127.0.0.1:8000/admin/manage/users/remove/' + benefactor.username + "/",
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            if (data.status == 0) {
+                alert(data.message);
+            } else {
+                alert("not success");
+            }
+        },
+        error: function (jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                msg = 'Not connect. Verify Network. [0]';
+            } else if (jqXHR.status === 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status === 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            alert(msg);
+        }
+
+    });
+}
+
+function verifyUser(benefactor) {
+    $.ajax({
+        async: true,
+        url: 'http://127.0.0.1:8000/admin/manage/users/verify/' + benefactor.username + "/",
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            if (data.status == 0) {
+                alert(data.message);
+            } else {
+                alert("not success");
+            }
+        },
+        error: function (jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                msg = 'Not connect. Verify Network. [0]';
+            } else if (jqXHR.status === 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status === 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            alert(msg);
+        }
+
+    });
+}
